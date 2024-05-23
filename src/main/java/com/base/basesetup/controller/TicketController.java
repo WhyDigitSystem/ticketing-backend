084b90ce44975f4cc8e4a950da54d875d2c4942c
Aravinth <justinaravinth@whydigit.com>
2024-05-23 11:14:26 +0530

    changes

--- src/main/java/com/base/basesetup/controller/TicketController.java
+++ src/main/java/com/base/basesetup/controller/TicketController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartFile; 
 
 import com.base.basesetup.common.CommonConstant;
 import com.base.basesetup.common.UserConstants;
